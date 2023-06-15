772fb6415d2ec443334b2e14c0a7c9dfcac27850
K.Harisankar <harisankar.krishna@outlook.com>
2023-06-15 17:40:34 +0800

    fix regression test fail for expires_in

--- src/tests/test_authentication_api_key_secret.py
+++ src/tests/test_authentication_api_key_secret.py
@@ -407,27 +407,29 @@ class TestHMACAuthentication(APITestCase):
         )
 
     def test_hmac_authentication__expires_in(self):
-        initial_datetime = datetime.now(timezone.utc)
-        test_expires_at = initial_datetime + timedelta(days=1)
+        initial_time = datetime.now(timezone.utc)
+        test_expires_at = initial_time - timedelta(days=1)
+        self.hmac_key.expires_at = test_expires_at
+        self.hmac_key.save()
+        self.hmac_key.refresh_from_db()
         factory = APIRequestFactory()
         req_data = ''
         with mock.patch(
             'django_hmac_authentication.authentication.hmac_expires_in',
             '1d',
         ):
-            with freeze_time(test_expires_at):
-                signature, utc_8601 = self._request_auth_header_fields(
-                    req_data, 'HMAC-SHA512'
-                )
-                headers = {
-                    f'{self.auth_header}': f'HMAC-SHA512 {self.hmac_key.id};{signature};{utc_8601}',
-                    'Content-Type': 'application/json',
-                }
-                request = factory.get('/', data=None, **headers)
-                response = self.view(request)
-                self.assertEqual(
-                    response.status_code,
-                    HTTPStatus.FORBIDDEN,
-                    'Expired key must fail authentication',
-                )
-                self._assert_response_error_detail(response.data, ExpiredKeyException())
+            signature, utc_8601 = self._request_auth_header_fields(
+                req_data, 'HMAC-SHA512'
+            )
+            headers = {
+                f'{self.auth_header}': f'HMAC-SHA512 {self.hmac_key.id};{signature};{utc_8601}',
+                'Content-Type': 'application/json',
+            }
+            request = factory.get('/', data=None, **headers)
+            response = self.view(request)
+            self.assertEqual(
+                response.status_code,
+                HTTPStatus.FORBIDDEN,
+                'Expired key must fail authentication',
+            )
+            self._assert_response_error_detail(response.data, ExpiredKeyException())
